d78c48f343e77d0c8112d6f068f4a06c4d74871d
Mike Hamilton <mike.hamilton7@gmail.com>
2024-02-05 02:00:19 -0700

    Setup doc styling

--- cfetcher.c
+++ cfetcher.c
@@ -1,5 +1,19 @@
 #include <curl/curl.h>
 
+/**
+ * @brief Initializes cURL
+ * 
+ */
+void initialize(){
+  curl_global_init(CURL_GLOBAL_DEFAULT);
+}
+
+/**
+ * @brief Main body orchestration
+ * 
+ * @return int Status of main execution
+ */
 int main(){
+  initialize();
   return(1);
 }
