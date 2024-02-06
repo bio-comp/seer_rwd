93ea527f1e94470c2385fb0dda9dca8d39ebe305
Mike Hamilton <mike.hamilton7@gmail.com>
2024-02-06 00:12:25 -0700

    Adding status checks to rust fetcher

--- rfetcher.rs
+++ rfetcher.rs
@@ -1,5 +1,5 @@
 use reqwest;
-
+// Working from: https://blog.logrocket.com/making-http-requests-rust-reqwest/
 async fn main(){
     let endpoint = "https://api.seer.cancer.gov/rest/disease/latest/"
     let SEER_API_SPEC = "X-SEERAPI-Key"
@@ -10,4 +10,21 @@ async fn main(){
     .get(endpoint)
     .header(SEER_API_SPEC, key)
     .send()
+    .await;
+
+    match response.status() {
+        reqwest::StatusCode::OK => {
+            // on success, parse our JSON to an APIResponse
+            match response.json::<APIResponse>().await {
+                Ok(parsed) => println!("Success! {:?}", parsed),
+                Err(_) => println!("Hm, the response didn't match the shape we expected."),
+            };
+        }
+        reqwest::StatusCode::UNAUTHORIZED => {
+            println!("Need to grab a new token");
+        }
+        other => {
+            panic!("Uh oh! Something unexpected happened: {:?}", other);
+        }
+    };
 }   
