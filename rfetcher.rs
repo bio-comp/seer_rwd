91e035c39998b8b2774424a6add8f6f2dfa83abc
Mike Hamilton <mike.hamilton7@gmail.com>
2024-02-08 23:54:46 -0700

    Adding status checks to rust fetcher

--- rfetcher.rs
+++ rfetcher.rs
@@ -3,7 +3,7 @@ use reqwest;
 async fn main(){
     let endpoint = "https://api.seer.cancer.gov/rest/disease/latest/"
     let SEER_API_SPEC = "X-SEERAPI-Key"
-    let key = ""
+    let key = "" // TODO: read key from python module
     let client = reqwest::Client::new();
 
     let response = client
